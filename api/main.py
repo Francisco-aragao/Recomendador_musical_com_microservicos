3cb57983961f6eaa432df1cc7886ffd2aae59690
probablygab <gabriel632@protonmail.com>
2023-11-25 21:41:32 +0000

    fix cors

--- api/main.py
+++ api/main.py
@@ -14,9 +14,15 @@ class RequestSongs(BaseModel):
 
 app = FastAPI()
 
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
